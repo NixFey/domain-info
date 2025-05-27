9f864b69854fdc29d5d1c6c3f309669fe69feefd
Nix <git@starnix.dev>
2025-05-27 14:41:47 -0400

    reorder button sections

--- app/domain/[domain]/components/dns-info.tsx
+++ app/domain/[domain]/components/dns-info.tsx
@@ -30,15 +30,6 @@ export default function DnsInfo({ initialRecords, updateRecordsAction }: { initi
   return (
     <Section title="DNS Records" id="dns-info" hotkey="d" ref={sectionRef}>
       <div className="mb-2 mx-[-4]">
-        <p className="px-2 font-bold">Deep/Shallow</p>
-        <Button active={deep} onclick={() => {
-          setDeep(true);
-        }}>All DNS Servers</Button>
-        <Button active={!deep} onclick={() => {
-          setDeep(false);
-        }}>Random DNS Server</Button>
-      </div>
-      <div className="mb-2 mx-[-4]">
         <p className="px-2 font-bold">Provider</p>
         <Button active={provider === "authoritative"} onclick={() => {
           setProvider("authoritative");
@@ -49,6 +40,15 @@ export default function DnsInfo({ initialRecords, updateRecordsAction }: { initi
         <Button active={provider === "cloudflare"} onclick={() => {
           setProvider("cloudflare");
         }}>Cloudflare</Button>
+      </div>
+      <div className="mb-2 mx-[-4]">
+        <p className="px-2 font-bold">Deep/Shallow</p>
+        <Button active={deep} onclick={() => {
+          setDeep(true);
+        }}>All DNS Servers</Button>
+        <Button active={!deep} onclick={() => {
+          setDeep(false);
+        }}>Random DNS Server</Button>
       </div>
       
       {loading && <Loading />}
