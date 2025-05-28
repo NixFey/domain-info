1760c40794ccf606c2e7e5d648b6bf18445f0100
Nix <git@starnix.dev>
2025-05-28 11:22:36 -0400

    monospace domain name

--- app/domain/[domain]/page.tsx
+++ app/domain/[domain]/page.tsx
@@ -27,7 +27,7 @@ export default async function DomainResult({ params }: DomainResultProps) {
   
   return (
     <article className="my-4 mb-200">
-      <h1 className="text-4xl text-center">{domain} <Link href="/" className="text-base"><span className="underline">New Search</span> <span className="text-mono bg-foreground/[0.1] border-1 border-foreground/[0.3] rounded-sm py-0.5 px-1">/</span></Link></h1>
+      <h1 className="text-4xl text-center"><span className="font-mono">{domain}</span> <Link href="/" className="text-base"><span className="underline">New Search</span> <span className="text-mono bg-foreground/[0.1] border-1 border-foreground/[0.3] rounded-sm py-0.5 px-1">/</span></Link></h1>
       {info
         ? <>
           <RegistrationInfo domainInfo={info} />
